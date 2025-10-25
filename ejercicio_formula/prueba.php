84b1bf4d3d1733f8f51027d7577260eeb78b1169
Udaeta <julen.udaeta00@somo.eus>
2025-10-25 19:19:50 +0200

    api tareminada

--- ejercicio_formula/prueba.php
+++ ejercicio_formula/prueba.php
@@ -1,9 +1,14 @@
 <?php
 
+$pilotos = [16,44];
+
+$numeros = implode("&", array_map(fn($n) => "driver_number=$n", $pilotos));
+$url = "https://api.openf1.org/v1/drivers?$numeros&session_key=9158";
+
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-    CURLOPT_URL => "https://api.openf1.org/v1/drivers?driver_number=1&session_key=9158",
+    CURLOPT_URL => $url,
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_FOLLOWLOCATION => true,
     CURLOPT_ENCODING => "",
@@ -20,9 +25,29 @@ curl_close($curl);
 
 if ($err) {
     echo "cURL Error #:" . $err;
-} else {
-    echo "<h2>Datos de pilotos seleccionados:</h2>";
-    echo "<pre>";
-    print_r(json_decode($response, true));
-    echo "</pre>";
+    exit;
 }
+
+$data = json_decode($response, true);
+
+echo "<h2>Datos de pilotos seleccionados</h2>";
+echo "<table border='1' cellpadding='6' cellspacing='0'>";
+echo "<tr>
+        <th>Número</th>
+        <th>Nombre completo</th>
+        <th>Equipo</th>
+        <th>Nacionalidad</th>
+        <th>Abreviatura</th>
+      </tr>";
+
+foreach ($data as $driver) {
+    echo "<tr>
+            <td>{$driver['driver_number']}</td>
+            <td>{$driver['full_name']}</td>
+            <td>{$driver['team_name']}</td>
+            <td>{$driver['country_code']}</td>
+            <td>{$driver['name_acronym']}</td>
+          </tr>";
+}
+
+echo "</table>";
